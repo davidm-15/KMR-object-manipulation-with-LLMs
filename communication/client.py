b1faf69f407119a34c273f1e3e5e905b75bf363c
Miroslav David <mirasira54123698745@gmail.com>
2025-02-11 13:33:09 +0100

    Fixed gitignore and image folder

--- communication/client.py
+++ communication/client.py
@@ -8,7 +8,7 @@ import numpy as np
 server_url = "http://localhost:5000/process"
 
 # Load or capture an image
-image_path = "image.jpg"  # Replace with actual image capture logic
+image_path = "images/image.jpg"  # Replace with actual image capture logic
 with open(image_path, "rb") as image_file:
     image_data = image_file.read()
 
@@ -31,17 +31,18 @@ try:
         processed_image_base64 = response_data["processed_image"]
         processed_image_bytes = base64.b64decode(processed_image_base64)
         processed_image = Image.open(io.BytesIO(processed_image_bytes))
-        processed_image.save("processed_image.jpg")
-        print("Processed image saved as 'processed_image.jpg'")
+        output_image_path = "images/processed_image.jpg"
+        processed_image.save(output_image_path)
+        print("Processed image saved as 'images/processed_image.jpg'")
 
         # Decode and save the predicted masks
         masks_base64 = response_data["pred_masks"]
         for i, mask_base64 in enumerate(masks_base64):
             mask_bytes = base64.b64decode(mask_base64)
             mask_image = Image.open(io.BytesIO(mask_bytes))
-            mask_image.save(f"pred_mask_{i}.jpg")
-            print(f"Predicted mask {i} saved as 'pred_mask_{i}.jpg'")
-    else:
-        print(f"Error: {response.status_code}, {response.text}")
+            output_mask_path = f"images/pred_mask_{i}.jpg"
+            mask_image.save(output_mask_path)
+            print(f"Predicted mask {i} saved as '{output_mask_path}'")
+                  
 except Exception as e:
     print(f"Failed to connect to the server: {e}")
